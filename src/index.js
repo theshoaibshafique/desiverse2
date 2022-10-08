bf2420de87f45a234a47a9c52f3f522fe274410c
M.Shoaib Shafiq <shoaibxp463@gmail.com>
2022-10-08 15:10:10 +0500

    MUI Setup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const mode = 'dark';
+
+let theme = createTheme({
+  palette: {
+    mode: mode,
+    // ...(mode === 'dark'
+    //   ? {
+    //       divider: '#1E2D3D',
+    //       background: {
+    //         default: '#011627',
+    //         paper: '#011627',
+    //       },
+    //     }
+    //   : {}),
+  },
+
+  typography: {
+    fontFamily: [
+      'Montserrat,sans-serif',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  // components: {
+  //   MuiPaper: {
+  //     styleOverrides: {
+  //       root: {
+  //         backgroundImage: 'none',
+  //       },
+  //     },
+  //   },
+  // },
+});
+
+theme = responsiveFontSizes(theme);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
