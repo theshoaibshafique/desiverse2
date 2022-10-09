f7302b997aa0084ecdf8574a3f9c3642e5da7315
M.Shoaib Shafiq <shoaibxp463@gmail.com>
2022-10-09 17:22:16 +0500

    lil fixes

--- src/components/Avatars.jsx
+++ src/components/Avatars.jsx
@@ -8,7 +8,7 @@ const Avatars = ({ item }) => {
     <Box
       sx={{
         height: '350px',
-        width: '220px',
+        width: '200px',
       }}
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
@@ -17,7 +17,7 @@ const Avatars = ({ item }) => {
         <img
           src={item.gif}
           alt=""
-          style={{ height: '100%', objectFit: 'cover', width: '220px' }}
+          style={{ height: '100%', objectFit: 'cover' }}
         />
       ) : (
         <img
